6ea063e8b3f1a5e55ccd494b77fea84b7ffcb996
xiedali <9727005@qq.com>
2017-11-25 10:20:30 +0800

    地方

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -55,7 +55,7 @@ return array(
     'DB_HOST' => '127.0.0.1', // 服务器地址
     'DB_NAME' => 'my_yidemember', // 数据库名
     'DB_USER' => 'root', // 用户名
-    'DB_PWD' => '', // 密码
+    'DB_PWD' => 'GQxB4LyasPhD8rqN', // 密码
     'DB_PORT' => '3306', // 端口
     'DB_PREFIX' => 'ott_', // 数据库表前缀
 
